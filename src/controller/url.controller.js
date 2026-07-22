b33a330af2039c96be7c2de043f574b49d44e4c5
Sagar Chauhan <sagarchauhan88558@gmail.com>
2026-07-22 23:18:23 +0530

    implemented the base62 encoding

--- src/controller/url.controller.js
+++ src/controller/url.controller.js
@@ -1,42 +1,75 @@
-const {nanoid} = require("nanoid");
 const URL = require("../models/url.model");
+const Counter = require("../models/counter.model");
+const { encode } = require("../services/base62");
 
-//post /shorten
+// POST /shorten
+exports.createShortUrl = async (req, res) => {
+    try {
+        const { longUrl } = req.body;
 
-exports.createShortUrl = async (req, res)=>{
-    try{
-        const {longUrl} = req.body;
-
-        if(!longUrl){
-            return res.status(400).json({message:"long url is required"});
+        if (!longUrl) {
+            return res.status(400).json({
+                message: "Long URL is required",
+            });
         }
-        const shortId = nanoid(6);
+
+        // Generate the next sequence number
+        const counter = await Counter.findOneAndUpdate(
+            { _id: "urlCounter" },
+            { $inc: { seq: 1 } },
+            {
+                new: true,
+                upsert: true,
+            }
+        );
+
+        // Convert sequence number to Base62
+        const shortId = encode(counter.seq);
+
+        // Save URL
         const newUrl = await URL.create({
-            shortId, longUrl,
+            shortId,
+            longUrl,
         });
 
         const shortUrl = `${process.env.BASE_URL}/${shortId}`;
 
         res.status(201).json({
-            shortUrl, longUrl:newUrl.longUrl,
+            message: "Short URL created successfully",
+            shortUrl,
+            shortId,
+            longUrl: newUrl.longUrl,
         });
-    }catch(err){
-        res.status(500).json({message: "server error"});
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
     }
 };
 
-//get /:shortId
+// GET /:shortId
+exports.redirectUrl = async (req, res) => {
+    try {
+        const { shortId } = req.params;
 
-exports.redirectUrl = async(req, res)=>{
-    try{
-        const{shortId} = req.params;
-        const url = await URL.findOne({shortId});
+        const url = await URL.findOne({ shortId });
 
-        if(!url){
-            return res.status(404).json({message: " URL not Found"});
+        if (!url) {
+            return res.status(404).json({
+                message: "URL not found",
+            });
         }
-        res.redirect(url.longUrl);
-    }catch(error){
-        res.status(500).json({message : "server error"});
+
+        return res.redirect(url.longUrl);
+
+    } catch (error) {
+        console.error(error);
+
+        res.status(500).json({
+            message: "Internal Server Error",
+        });
     }
-}
+};
